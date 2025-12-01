2f7a9b08f188523b0474ec865408cb2f1852edb7
Emil Nielsen <emil0306dk@gmail.com>
2025-12-01 19:44:27 +0100

    Day 1 first part done

---
+++ adventofcode2025/day_1_Secret_Entrance/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program{
+    static void Main(){
+        TextReader? tr = null;
+        string currentLine = "";
+        bool done = false;
+        int pointer = 50;
+        int code = 0;
+        try {
+            tr = new StreamReader(System.IO.File.OpenRead("input.txt"));
+            while (!done){
+                currentLine = tr.ReadLine();
+                string[] number;
+                switch(currentLine[0])
+                {
+                    case 'R':
+                        number = currentLine.Split("R");
+                        pointer += int.Parse(number[1]);
+                        if (pointer % 100 == 0){
+                            code++;
+                        }
+                        break;
+                    case 'L':
+                        number = currentLine.Split("L");
+                        pointer -= int.Parse(number[1]);
+                        if (pointer % 100 == 0){
+                            code++;
+                        }
+                        break;
+                }
+            }
+        }
+        catch(DirectoryNotFoundException){
+            Console.WriteLine("Invalid path");
+        }
+        catch(UnauthorizedAccessException){
+            Console.WriteLine("You dont have acces to the file");
+        }
+        catch (System.NullReferenceException){ // Done with file
+            Console.WriteLine("The code is: "+code);
+        }
+        catch(Exception ex){
+            Console.WriteLine("Something went wrong "+ex);
+        }
+        finally{
+            tr?.Close(); // allways close file
+        }
+    }
+}
