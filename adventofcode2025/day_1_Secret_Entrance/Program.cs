472bf0f7bc7221dde401a2fad74aa95e75773ef0
Emil Nielsen <emil0306dk@gmail.com>
2025-12-03 00:38:44 +0100

    Day 1: part 2 done :)

--- adventofcode2025/day_1_Secret_Entrance/Program.cs
+++ adventofcode2025/day_1_Secret_Entrance/Program.cs
@@ -6,25 +6,39 @@
         int pointer = 50;
         int code = 0;
         try {
-            tr = new StreamReader(System.IO.File.OpenRead("input.txt"));
-            while (!done){
+            tr = new StreamReader(System.IO.File.OpenRead("C:\\github_repo\\adventofcode\\adventofcode2025\\day_1_Secret_Entrance\\input.txt"));
+            while (true){
                 currentLine = tr.ReadLine();
+                if (currentLine == null) break;
                 string[] number;
+                int oldPointer = pointer;
                 switch(currentLine[0])
                 {
                     case 'R':
                         number = currentLine.Split("R");
                         pointer += int.Parse(number[1]);
-                        if (pointer % 100 == 0){
-                            code++;
+                        if (pointer >= 100)
+                        {
+                            do
+                            {
+                                pointer -= 100;
+                                code++;
+                            } while (pointer >= 100);
                         }
                         break;
                     case 'L':
                         number = currentLine.Split("L");
                         pointer -= int.Parse(number[1]);
-                        if (pointer % 100 == 0){
-                            code++;
+                        if (pointer < 0)
+                        {
+                            do
+                            {
+                                pointer += 100;
+                                code++;
+                            } while (pointer < 0);
                         }
+                        if (pointer == 0) code++;
+                        if (oldPointer == 0) code--;
                         break;
                 }
             }
@@ -35,14 +49,12 @@
         catch(UnauthorizedAccessException){
             Console.WriteLine("You dont have acces to the file");
         }
-        catch (System.NullReferenceException){ // Done with file
-            Console.WriteLine("The code is: "+code);
-        }
         catch(Exception ex){
             Console.WriteLine("Something went wrong "+ex);
         }
         finally{
             tr?.Close(); // allways close file
         }
+        Console.WriteLine("The code is: "+code);
     }
 }
